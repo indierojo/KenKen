0ea782cbc73d80a16dbb785f14c0b9bd289e33b7
Jon Bullock <jondooger@gmail.com>
2014-01-01 22:29:48 -0700

    Added GroupDefinition object.

---
+++ KenKen/Domain/GroupDefinition.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain
+{
+    public class GroupDefinition
+    {
+        public GroupDefinition(ushort groupNumber, IOperation operation, ushort expectedTotal)
+        {
+            GroupNumber = groupNumber;
+            ExpectedTotal = expectedTotal;
+            Operation = operation;
+        }
+
+        public ushort GroupNumber { get; private set; }
+        public IOperation Operation { get; private set; }
+        public ushort ExpectedTotal { get; private set; }
+
+        public override string ToString()
+        {
+            return ExpectedTotal + Operation.GetSymbol();
+        }
+    }
+}
