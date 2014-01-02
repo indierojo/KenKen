f3bbbfdb1cd8306400b468f919bf01605cda9390
Jon Bullock <jondooger@gmail.com>
2014-01-01 18:28:55 -0700

    Added cell class.

---
+++ KenKen/Domain/Cell.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain
+{
+    public class Cell
+    {
+        public Cell(ushort x, ushort y, ushort group, ushort? value = null)
+        {
+            X = x;
+            Y = y;
+            Group = group;
+            Value = value;
+        }
+
+        public ushort X { get; private set; }
+        public ushort Y { get; private set; }
+        public ushort Group { get; private set; }
+
+        public ushort? Value { get; set; }
+    }
+}
