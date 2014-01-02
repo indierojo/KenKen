ac29d54f82a7dadb7e0ac0231a950a5319ce4e7d
Jon Bullock <jondooger@gmail.com>
2014-01-01 18:32:02 -0700

    Added rudimentary Grid class.

---
+++ KenKen/Domain/Grid.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class Grid
+    {
+        public Grid(int size)
+        {
+            GridCells = new Cell[size, size];
+        }
+
+        public Cell[,] GridCells { get; private set; }
+    }
+}
