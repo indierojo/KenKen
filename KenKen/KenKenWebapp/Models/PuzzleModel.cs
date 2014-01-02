5c1aca5f085e8649ff5d542654dd0e7072dc8f8b
Jon Bullock <jondooger@gmail.com>
2014-01-01 23:04:26 -0700

    Added wrapper class for PuzzleModel.

---
+++ KenKen/KenKenWebapp/Models/PuzzleModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+
+namespace KenKenWebapp.Models
+{
+    public class PuzzleModel
+    {
+        public PuzzleModel(Puzzle puzzle)
+        {
+            Puzzle = puzzle;
+        }
+
+        public Puzzle Puzzle { get; set; }
+    }
+}
