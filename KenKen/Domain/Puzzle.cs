dee1e3cb38d98cae604bee2781d4cf24b8644c59
Jon Bullock <jondooger@gmail.com>
2014-01-01 22:30:17 -0700

    Added group definitions to puzzle object.

--- KenKen/Domain/Puzzle.cs
+++ KenKen/Domain/Puzzle.cs
@@ -1,15 +1,27 @@
-﻿namespace Domain
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain
 {
     public class Puzzle
     {
-        public Puzzle(Cell[,] cells)
+        public readonly Dictionary<ushort, GroupDefinition> Groups;
+        private ushort? _gridDimensions;
+
+        public Puzzle(Cell[,] cells, IEnumerable<GroupDefinition> groups)
         {
+            Groups = groups.ToDictionary(g => g.GroupNumber);
             GridCells = cells;
         }
 
-        ushort GetGridDimensions()
+        public ushort GetGridDimensions()
         {
-            return (ushort) GridCells.GetLength(0);
+            if (!_gridDimensions.HasValue)
+            {
+                _gridDimensions = (ushort) GridCells.GetLength(0);
+            }
+
+            return _gridDimensions.Value;
         }
 
         public Cell[,] GridCells { get; set; }
