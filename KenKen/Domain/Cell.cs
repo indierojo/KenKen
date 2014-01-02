7204dda7126f381d315b9c4a26ff8bcf6582549b
Jon Bullock <jondooger@gmail.com>
2014-01-01 19:01:14 -0700

    Removed X, Y from Cell.

--- KenKen/Domain/Cell.cs
+++ KenKen/Domain/Cell.cs
@@ -2,16 +2,11 @@
 {
     public class Cell
     {
-        public Cell(ushort x, ushort y, ushort group, ushort? value = null)
+        public Cell(ushort group, ushort? value = null)
         {
-            X = x;
-            Y = y;
             Group = group;
             Value = value;
         }
-
-        public ushort X { get; private set; }
-        public ushort Y { get; private set; }
         public ushort Group { get; private set; }
 
         public ushort? Value { get; set; }
