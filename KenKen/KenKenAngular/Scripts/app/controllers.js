789210f2dd3a935288750412e8653872dc0c6bb8
Jon Bullock <jon@talentriot.com>
2014-01-26 10:20:00 -0700

    Extracted border population to a method. Simplified border population.

--- KenKen/KenKenAngular/Scripts/app/controllers.js
+++ KenKen/KenKenAngular/Scripts/app/controllers.js
@@ -7,41 +7,7 @@ kenkenApp.config([
 ]);
 kenkenApp.controller('kenkenApp', ['$scope', '$http', function ($scope, $http) {
     $http.get('http://localhost:63995/api/puzzle/1').success(function (data) {
-        var gridSize = data.Grid.Cells.length;
-        var seenGroups = [];
-
-        var cells = data.Grid.Cells;
-
-        for (var x = 0; x < gridSize; x++) {
-            for (var y = 0; y < gridSize; y++) {
-                var cell = cells[x][y];
-                if (seenGroups.indexOf(cell.Group) === -1) {
-                    seenGroups.push(cell.Group);
-                    cell.showSymbol = true;
-                }
-
-                //   [0,0][0,1][0,2][0,3]
-                //   [1,0][1,1][1,2][1,3]
-                //   [2,0][2,1][2,2][2,3]
-                //   [3,0][3,1][3,2][3,3]
-
-                // cell.x = x;
-                // cell.y = y;
-
-                if (x > 0 && cells[x - 1][y].Group !== cell.Group) {
-                    cell.topBorder = true;
-                }
-                if (y > 0 && cells[x][y - 1].Group !== cell.Group) {
-                    cell.leftBorder = true;
-                }
-                if (y < gridSize - 1 && cells[x][y + 1].Group !== cell.Group) {
-                    cell.rightBorder = true;
-                }
-                if (x < gridSize - 1 && cells[x + 1][y].Group !== cell.Group) {
-                    cell.bottomBorder = true;
-                }
-            }
-        }
+        populateCellBorderData(data);
 
         $scope.puzzle = data;
         $scope.getBorderClasses = function(cell) {
@@ -68,3 +34,41 @@ kenkenApp.filter('groupSymbol', function() {
         return symbol;
     };
 });
+
+var populateCellBorderData = function(data) {
+    var gridSize = data.Grid.Cells.length;
+    var seenGroups = [];
+
+    var cells = data.Grid.Cells;
+
+    for (var x = 0; x < gridSize; x++) {
+        for (var y = 0; y < gridSize; y++) {
+            var cell = cells[x][y];
+            if (seenGroups.indexOf(cell.Group) === -1) {
+                seenGroups.push(cell.Group);
+                cell.showSymbol = true;
+            }
+
+            //   [0,0][0,1][0,2][0,3]
+            //   [1,0][1,1][1,2][1,3]
+            //   [2,0][2,1][2,2][2,3]
+            //   [3,0][3,1][3,2][3,3]
+
+            // cell.x = x;
+            // cell.y = y;
+
+            if (x > 0 && cells[x - 1][y].Group !== cell.Group) {
+                cell.topBorder = true;
+            }
+            if (y > 0 && cells[x][y - 1].Group !== cell.Group) {
+                cell.leftBorder = true;
+            }
+            if (y < gridSize - 1 && cells[x][y + 1].Group !== cell.Group) {
+                cell.rightBorder = true;
+            }
+            if (x < gridSize - 1 && cells[x + 1][y].Group !== cell.Group) {
+                cell.bottomBorder = true;
+            }
+        }
+    }
+}
