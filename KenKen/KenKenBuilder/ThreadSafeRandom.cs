0eb86d11592e110008f2da97e7af3fada49a0aaf
Jon Bullock <jon@talentriot.com>
2014-02-05 13:34:24 -0600

    Added ThreadSafeRandom class.

---
+++ KenKen/KenKenBuilder/ThreadSafeRandom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KenKenBuilder
+{
+    public class ThreadSafeRandom
+    {
+        private static readonly Random Global = new Random();
+        [ThreadStatic] private static Random _local;
+
+        public ThreadSafeRandom()
+        {
+            if (_local == null)
+            {
+                int seed;
+                lock (Global)
+                {
+                    seed = Global.Next();
+                }
+                _local = new Random(seed);
+            }
+        }
+
+        public int Next()
+        {
+            return _local.Next();
+        }
+
+        public double NextDouble()
+        {
+            return _local.NextDouble();
+        }
+    }
+}
